f5a91027375bf4330e3833724be342115c1fae7a
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-11-05 08:59:24 +0800

    Detect user dark mode preference

--- example/js/main.js
+++ example/js/main.js
@@ -1,11 +1,16 @@
 document.title = 'HTML Boilerplate with Tailwind'
 
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
 const toggleButton = document.querySelector('#dark-mode-toggle')
+
+if (prefersDarkScheme) {
+  document.documentElement.classList.add('dark')
+}
 
 toggleButton?.addEventListener('click', () => {
   const isDark = document.documentElement.classList.contains('dark')
 
-  if (isDark) {
+  if (isDark || (!isDark && !prefersDarkScheme)) {
     document.documentElement.classList.remove('dark')
     toggleButton.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="moon" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></svg>
